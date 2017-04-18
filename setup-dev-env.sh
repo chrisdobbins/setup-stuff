5e86f5683b5df659215bd2c4b8558eb28f052e9e
Chris <chrisdobbins@users.noreply.github.com>
2017-04-18 12:55:22 +0000

    Update setup-dev-env.sh

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -10,12 +10,19 @@ fi
 
 apt-get update
 apt-get install -y git
+apt-get install curl
 
 wget https://storage.googleapis.com/golang/go1.8.1.linux-386.tar.gz
 echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.profile
 tar -C /usr/local -xzf go1.8.1.linux-386.tar.gz
 rm go1.8.1.linux-386.tar.gz
-
 mkdir -p $HOME/go/src 
 chmod -R 777 $HOME/go/src
+
+cd ~/Downloads
+wget https://raw.githubusercontent.com/xlucas/go-vim-install/master/install.sh
+chmod +x install.sh
+./install.sh -vim
+./install.sh -work $HOME/go/src
+
 exit 0
