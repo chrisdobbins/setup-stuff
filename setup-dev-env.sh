1d846ee6ca42f6ca87014d2eafd0f6dbf3d3dc27
Chris <chrisdobbins@users.noreply.github.com>
2017-07-29 23:41:01 +0000

    Update setup-dev-env.sh

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -13,19 +13,19 @@ sudo apt-get update
 # will allow restricted/proprietary media formats to be played...and a bunch of other stuff
 # sudo apt-get install -y ubuntu-restricted-extras
 
-"hash git" 2> /dev/null
+hash git 2> /dev/null
  if [ $? -ne 0 ]; then
        echo 'Installing git...'
        apt-get install -y git
 fi
 
-"hash curl" 2> /dev/null
+hash curl 2> /dev/null
 if [ $? -ne 0 ]; then
        echo 'Installing curl...'
        apt-get install -y curl
 fi
 
-"hash go" 2> /dev/null
+hash go 2> /dev/null
 if [ $? -ne 0 ]; then
   echo 'Installing Go...'
 # os arch is 32-bit  
@@ -46,9 +46,11 @@ if [ $? -ne 0 ]; then
 fi
 
 echo 'Installing nvm...'
-wget -q0- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> $HOME/.profile
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+) && . "$NVM_DIR/nvm.sh"
 
 echo 'Installing vim...'
 cd ~/Downloads
@@ -58,6 +60,13 @@ chmod +x install.sh
 ./install.sh -work $HOME/go/src
 rm install.sh
 apt-get install -y vim
+
+if [ -z $(uname -m | grep 64) ]; then
+  echo 'Installing Visual Studio Code...'
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+fi
 
 echo 'All done! Remember to run . '"${!HOME}"'/.profile!'
 
