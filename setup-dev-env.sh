f1155d712b1e4348d9579ebf7419453417936990
Chris <chrisdobbins@users.noreply.github.com>
2017-08-10 03:04:18 -0400

    Update setup-dev-env.sh

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -7,8 +7,8 @@ echo "must be root to run this"
 exit 1
 fi
 
-# sudo add-apt-repository universe && sudo add-apt-repository multiverse
-sudo apt-get update
+add-apt-repository universe && add-apt-repository multiverse && add-apt-repository main
+apt-get update
 
 # will allow restricted/proprietary media formats to be played...and a bunch of other stuff
 # sudo apt-get install -y ubuntu-restricted-extras
@@ -45,29 +45,37 @@ if [ $? -ne 0 ]; then
   chmod -R 777 $HOME/go/src
 fi
 
-echo 'Installing nvm...'
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
-) && . "$NVM_DIR/nvm.sh"
+hash nvm 2> /dev/null
+if [ $? -ne 0 ]; then
+  echo 'Installing nvm...'
+  export NVM_DIR="$HOME/.nvm" && (
+    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  ) && . "$NVM_DIR/nvm.sh"
+fi
 
-echo 'Installing vim...'
-cd ~/Downloads
-wget https://raw.githubusercontent.com/xlucas/go-vim-install/master/install.sh
-chmod +x install.sh
-./install.sh -vim
-./install.sh -work $HOME/go/src
-rm install.sh
-apt-get install -y vim
+hash vim 2> /dev/null
+if [ $? -ne 0 ]; then
+  echo 'Installing vim...'
+  cd ~/Downloads
+  wget https://raw.githubusercontent.com/xlucas/go-vim-install/master/install.sh
+  chmod +x install.sh
+  ./install.sh -vim
+  ./install.sh -work $HOME/go/src
+  rm install.sh
+  apt-get install -y vim
+fi
 
+# just assuming VS Code isn't installed...
 if [ -z $(uname -m | grep 64) ]; then
   echo 'Installing Visual Studio Code...'
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  apt-get update && apt install code
 fi
 
-echo 'All done! Remember to run . '"${!HOME}"'/.profile!'
+echo 'All done!'
 
 exit 0
